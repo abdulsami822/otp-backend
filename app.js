95ed51e541b26edabb6acf03febc49cae273ebec
abdulsami <samiabdul822@gmail.com>
2022-09-20 16:43:48 +0530

    Fetch Contacts API
    Implemented Fetch Contacts API

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const port = process.env.PORT;
 const apiKey = process.env.VONAGE_API_KEY;
 const apiSecret = process.env.VONAGE_API_SECRET;
 
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+let db = null;
+const dbFilePath = path.join(__dirname, "message.db");
 
 const Vonage = require("@vonage/server-sdk");
 const vonage = new Vonage({
@@ -15,8 +21,12 @@ const vonage = new Vonage({
 });
 
 //start server
-const initializeServer = () => {
+const initializeServer = async () => {
   try {
+    db = await open({
+      filename: dbFilePath,
+      driver: sqlite3.Database,
+    });
     app.listen(port, () => {
       console.log(`server is running at ${port}`);
     });
@@ -52,13 +62,15 @@ const checkRequestBody = (request, response, next) => {
 app.post("/send-otp", checkRequestBody, async (request, response) => {
   try {
     const from = "sami";
-    const { to, text } = request.body;
+    const { name, to, text, otp } = request.body;
 
-    vonage.message.sendSms(from, to, text, (err, response) => {
+    vonage.message.sendSms(from, to, text, async (err, response) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(response);
+        //store otp in db
+        const query = `INSERT INTO otp(name,time,otp) VALUES ("${name}",CURRENT_TIMESTAMP,${otp})`;
+        await db.run(query);
       }
     });
 
@@ -68,5 +80,17 @@ app.post("/send-otp", checkRequestBody, async (request, response) => {
     console.log(error.message);
     response.status(500);
     response.send({ err: "unexpected error has occured" });
+  }
+});
+
+app.get("/contacts", async (request, response) => {
+  try {
+    const query = `select * from contact`;
+    const contacts = await db.all(query);
+    response.status(200);
+    response.send(contacts);
+  } catch (error) {
+    response.status(500);
+    response.send("Something Went Wrong");
   }
 });
